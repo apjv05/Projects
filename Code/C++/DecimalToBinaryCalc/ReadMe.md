008276977515f933c7cae1882470a57cb9e7b9ad
Andrew <andrewpvoyez@gmail.com>
2025-05-01 23:34:15 -0500

    Updated Function & added ReadMe

---
+++ Code/C++/DecimalToBinaryCalc/ReadMe.md
@@ -0,0 +1,2 @@
+# Binary to Decimal Calculator
+This project is a work in progress. I'm planning on turning it into a converter for Decimal, Binary, and Hexadecimal numbers in the future.
--- Code/C++/DecimalToBinaryCalc/main.cpp
+++ Code/C++/DecimalToBinaryCalc/main.cpp
@@ -2,55 +2,34 @@
 #include <string>
 #include <algorithm>
 
+using namespace std;
+
 //Purpose: Turn a decimal number into binary using recursion
 
-std::string BinaryCalc(int InputNumber, std::string ConvertedNumber) {
-    //if the input number equals 0 it returns the number in binary
+string BinaryCalc(int InputNumber, string ConvertedNumber) {
     if(InputNumber == 0) {
         //reverses the ConvertedNumber variable to correct it
-        std::reverse(ConvertedNumber.begin(), ConvertedNumber.end());
+        reverse(ConvertedNumber.begin(), ConvertedNumber.end());
         return ConvertedNumber;
     };
 
-    int NewNumber, Remainder;
+    int Remainder;
 
-    //calculate new number
-    NewNumber = InputNumber / 2;
-
-    //find out what the remainder is
     Remainder = InputNumber % 2;
 
-    //if it is one add one to the end of the string and loop
-    if(Remainder == 1) {
-        //convert Remainder into a string because append only accepts strings
-        std::string sRemainder = std::to_string(Remainder);
+    string sRemainder = to_string(Remainder);
+    ConvertedNumber.append(sRemainder);
 
-        //put the remainder at the end of the converted number
-        ConvertedNumber.append(sRemainder);
-
-        //recursion
-        return BinaryCalc(NewNumber, ConvertedNumber);
-    }
-    //if it is zero add zero to the end of the string and loop
-    else {
-        //convert Remainder into a string because append only accepts strings
-        std::string sRemainder = std::to_string(Remainder);
-
-        //put the remainder at the end of the converted number
-        ConvertedNumber.append(sRemainder);
-
-        //recursion
-        return BinaryCalc(NewNumber, ConvertedNumber);
-    }
+    return BinaryCalc(InputNumber / 2, ConvertedNumber);
 }
 
 int main() {
     int UserNumber;
-    std::string BinaryNumber;
+    string BinaryNumber;
 
-    std::cout << "What number to do you want to translate into binary?: ";
-    std::cin >> UserNumber;
-    std::cout << UserNumber << " in binary is: " << BinaryCalc(UserNumber, BinaryNumber);
+    cout << "What number to do you want to translate into binary?: ";
+    cin >> UserNumber;
+    cout << UserNumber << " in binary is: " << BinaryCalc(UserNumber, BinaryNumber);
 
     return 0;
 }
